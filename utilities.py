ded27c80531a60876dea41d9e21a5cc233005c29
Supreeth Keragodu Suryaprakash <skeragod@silo.soic.indiana.edu>
2016-12-09 22:48:55 -0500

    intensity using anup

--- utilities.py
+++ utilities.py
@@ -1,7 +1,8 @@
 '''
 This file contains the utility functions that can be used through out the assignment.
 '''
-
+import random
+import string
 
 def readFile(fileName):
 	file = open(fileName, 'r')
@@ -12,15 +13,16 @@ def readFile(fileName):
 
 def convertToRGB(fileName):
 	RGBList = dict()
+	ctr = 0
 	contents = readFile(fileName)
 	for eachLine in contents:
-		rgbValues = ()
+		rgbValues = () 
 		imageVector = eachLine.split(' ')
 		for i in range(2, len(imageVector), 3):
-			rgbValues = rgbValues + (map(int, imageVector[i:i+3]), )
-		RGBList[imageVector[0]] = (int(imageVector[1]), rgbValues)
+			rgbValues = rgbValues + (findIntensity(map(int,imageVector[i:i+3])), )
+		RGBList[ctr] = (int(imageVector[1]), rgbValues)
+		ctr += 1
 	return RGBList
 
-
-
-
+def findIntensity(pixel):
+	return (0.2989 * pixel[0]) + (0.5870 * pixel[1]) + (0.1140 * pixel[2])
