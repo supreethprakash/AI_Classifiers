a1fd93097bf37414de9bed0a9c60a10c73759f9e
rkanchib <rkanchib@iu.edu>
2016-12-10 05:59:19 -0500

    adaboost: choosing random points. accuracy varying between 15-30 %

--- adaboost.py
+++ adaboost.py
@@ -1,55 +1,128 @@
 from utilities import *
 import random
+import operator
+
+
+def calculateBestSplit(datatset, degree, weight, ensemble):
+    dataLength = len(datatset[0][1])
+    x, y = random.sample(set(list(range(dataLength))), 2)
+    leastError = 100000
+    if x != y:
+        error = 0
+        counter = 0
+        tempWeights = weight
+        for records in datatset.values():
+            tempDegree = 1
+            if weight[counter] == 1:
+                if records[1][x] > records[1][y]:
+                    tempDegree = degree
+                if tempDegree == records[0]:
+                    tempWeights[counter] = 0
+                else:
+                    error += 1
+            counter += 1
+        if error < leastError:
+            bestAtt1 = x
+            bestAtt2 = y
+            if (bestAtt1, bestAtt2) not in ensemble:
+                alpha = float(1 - (float(error) / len(datatset))) / len(datatset)
+                alpha = alpha * 100000
+                l = []
+                l.append(alpha)
+                l.append((x, y))
+                ensemble.append(l)
+        else:
+            calculateBestSplit(dataset,degree,weight,ensemble)
+    return ensemble
+
 
 
 def ensemle(dataset, numOfStumps, degree):
-    counter1 = 0
-    ensemble0 = []
-    x, y = 2, 4
-    for i in range(numOfStumps):
-        weights = [1] * len(dataset)
-        bestSplit = []
-        bestCounter = 0
-        bestError = 0
-        counter1 += 1
-        counter = 0
-        error = 0
-        counter2 = 0
-        for j in dataset.values():
-            if weights[counter2] == 1:
-                z = 1
-                counter2 += 1
-                if j[1][x] > j[1][y]:
-                    z = degree
-                if int(j[0]) == z:
-                    counter += 1
-                    weights[counter2] = 0
-                else:
-                    error += 1
-                if counter > bestCounter:
-                    bestCounter = counter
-                    bestSplit = [x,y]
-                    bestError = error
-        alpha = float(1 - (float(bestError)/len(dataset)))/len(dataset)
-        alpha = alpha * 10000
-        x += 2
-        y += 3
-        l = []
-        l.append(alpha)
-        l.append(bestSplit)
-        ensemble0.append(l)
-    return ensemble0
 
+    ensemble = []
+    for i in range(numOfStumps):
+        ensemble = calculateBestSplit(dataset,degree,weight, ensemble)
+    return ensemble
+
+
+def testing(e0,e1,e2,e3,dataset):
+    correctness = 0
+    for record in dataset.values():
+        counterDict = {'counter0':0, 'counter1':0, 'counter2':0, 'counter3':0}
+        for i in range(len(e0)):
+            val = e0[i][0]
+            att = e0[i][1]
+            x = att[0]
+            y = att[1]
+            if record[1][x] > record[1][y]:
+                counterDict['counter0'] += 1
+            else:
+                counterDict['counter0'] -= 1
+        for i in range(len(e1)):
+            val = e1[i][0]
+            att = e1[i][1]
+            x = att[0]
+            y = att[1]
+            if record[1][x] > record[1][y]:
+                counterDict['counter1'] += 1
+            else:
+                counterDict['counter1'] -= 1
+        for i in range(len(e2)):
+            val = e2[i][0]
+            att = e2[i][1]
+            x = att[0]
+            y = att[1]
+            if record[1][x] > record[1][y]:
+                counterDict['counter2'] += 1
+            else:
+                counterDict['counter2'] -= 1
+        for i in range(len(e3)):
+            val = e3[i][0]
+            att = e3[i][1]
+            x = att[0]
+            y = att[1]
+            if record[1][x] > record[1][y]:
+                counterDict['counter3'] += 1
+            else:
+                counterDict['counter3'] -= 1
+
+        maximumScore = sorted(counterDict.iteritems(), key=operator.itemgetter(1), reverse=True)
+        vote = maximumScore[0][0]
+        if vote == 'counter0':
+            c = 0
+        elif vote == 'counter1':
+            c = 90
+        elif vote == 'counter2':
+            c =180
+        else:
+            c =270
+        if c == int(record[0]):
+            correctness += 1
+
+    return correctness
 
 if __name__ == '__main__':
-    trainingSet=[]
-    testSet=[]
+    trainingSet = []
+    testSet = []
 
     testDict = convertToRGB('test-data.txt')
     trainingSet = convertToRGB('train-data.txt')
+    weight = [1] * len(trainingSet)
+    e0 = ensemle(trainingSet, 20, 0)
+    weight = [1] * len(trainingSet)
+    e1 = ensemle(trainingSet, 20, 90)
+    weight = [1] * len(trainingSet)
+    e2 = ensemle(trainingSet, 20, 180)
+    weight = [1] * len(trainingSet)
+    e3 = ensemle(trainingSet, 20, 270)
 
+    c = testing(e0,e1,e2,e3,testDict)
 
-    e0 = ensemle(trainingSet, 30,0)
-    e1 = ensemle(trainingSet, 30,90)
-    e2 = ensemle(trainingSet, 30,180)
-    e3 = ensemle(trainingSet, 30,270)
+    print e0
+    print e1
+    print e2
+    print e3
+
+    print c
+    print len(testDict)
+    print float(c)/len(testDict)
