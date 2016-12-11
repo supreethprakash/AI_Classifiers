6dd6c1928641c872d3ea7e731602aac957698371
rkanchib <rkanchib@iu.edu>
2016-12-10 21:55:14 -0500

    taking weights summing upto 1 instead of binary values

--- adaboost.py
+++ adaboost.py
@@ -3,48 +3,62 @@ import random
 import operator
 
 
-def calculateBestSplit(datatset, degree, weight, ensemble):
+def calculateBestSplit(datatset, degree, weight, ensemble, success, error):
     dataLength = len(datatset[0][1])
     x, y = random.sample(set(list(range(dataLength))), 2)
-    leastError = 40000
+    maxWeight = max(weight)
+    su = max(weight)
+    er = min(weight)
     if x != y:
-        error = 0
         counter = 0
-        tempWeights = weight
+        tempSuccess = 0
+        tempError = 0
         for records in datatset.values():
             tempDegree = 1
-            if weight[counter] == 1:
+            if weight[counter] == maxWeight:
                 if records[1][x] > records[1][y]:
                     tempDegree = degree
                 if tempDegree == records[0]:
-                    tempWeights[counter] = 0
+                    tempSuccess += 1
                 else:
-                    error += 1
+                    tempError += 1
             counter += 1
-        if error < leastError:
+        if tempSuccess >= success and tempError <= error:
+            counterNew = 0
+            success = tempSuccess
+            error = tempError
+            errorWeight = float(tempError)/len(datatset)
+            errorWeight = (er * errorWeight)/(1-errorWeight)
+            for records in datatset.values():
+                tempDegree = 1
+                if weight[counterNew] == maxWeight:
+                    if records[1][x] > records[1][y]:
+                        tempDegree = degree
+                    if tempDegree == records[0]:
+                        weight[counterNew] = errorWeight
+                counterNew += 1
             bestAtt1 = x
             bestAtt2 = y
             if (bestAtt1, bestAtt2) not in ensemble:
-                weight = tempWeights
                 alpha = float(1 - (float(error) / len(datatset))) / len(datatset)
                 alpha = alpha * 100000
                 l = []
                 l.append(alpha)
                 l.append((x, y))
                 ensemble.append(l)
-        else:
-            calculateBestSplit(dataset,degree,weight,ensemble)
     return ensemble
 
 
 
-def ensemle(dataset, numOfStumps, degree):
+def ensemle(dataset, numOfStumps, degree, weight, success, error):
 
     ensemble = []
-    #weight = [1] * len(dataset)
     for i in range(numOfStumps):
-        ensemble = calculateBestSplit(dataset,degree,weight, ensemble)
+        ensemble = calculateBestSplit(dataset,degree,weight,ensemble, success, error)
+        sumOfWeights = sum(weight)
+        weight = [float(i)/sumOfWeights for i in weight]
     return ensemble
+
 
 
 def testing(e0,e1,e2,e3,dataset):
@@ -57,36 +71,36 @@ def testing(e0,e1,e2,e3,dataset):
             x = att[0]
             y = att[1]
             if record[1][x] > record[1][y]:
-                counterDict['counter0'] += 1
+                counterDict['counter0'] += val * 1
             else:
-                counterDict['counter0'] -= 1
+                counterDict['counter0'] += val * -1
         for i in range(len(e1)):
             val = e1[i][0]
             att = e1[i][1]
             x = att[0]
             y = att[1]
             if record[1][x] > record[1][y]:
-                counterDict['counter1'] += 1
+                counterDict['counter1'] += val * 1
             else:
-                counterDict['counter1'] -= 1
+                counterDict['counter1'] += val * -1
         for i in range(len(e2)):
             val = e2[i][0]
             att = e2[i][1]
             x = att[0]
             y = att[1]
             if record[1][x] > record[1][y]:
-                counterDict['counter2'] += 1
+                counterDict['counter2'] += val * 1
             else:
-                counterDict['counter2'] -= 1
+                counterDict['counter2'] += val * -1
         for i in range(len(e3)):
             val = e3[i][0]
             att = e3[i][1]
             x = att[0]
             y = att[1]
             if record[1][x] > record[1][y]:
-                counterDict['counter3'] += 1
+                counterDict['counter3'] += val * 1
             else:
-                counterDict['counter3'] -= 1
+                counterDict['counter3'] += val * -1
 
         maximumScore = sorted(counterDict.iteritems(), key=operator.itemgetter(1), reverse=True)
         vote = maximumScore[0][0]
@@ -109,14 +123,21 @@ if __name__ == '__main__':
 
     testDict = convertToRGB('test-data.txt')
     trainingSet = convertToRGB('train-data.txt')
-    weight = [1] * len(trainingSet)
-    e0 = ensemle(trainingSet, 50, 0)
-    weight = [1] * len(trainingSet)
-    e1 = ensemle(trainingSet, 50, 90)
-    weight = [1] * len(trainingSet)
-    e2 = ensemle(trainingSet, 50, 180)
-    weight = [1] * len(trainingSet)
-    e3 = ensemle(trainingSet, 50, 270)
+    orientation = [0,90,180,270]
+    points = float(1)/len(trainingSet)
+    for i in orientation:
+        weight = [points] * len(trainingSet)
+        success = 0
+        error = len(trainingSet)
+        e = ensemle(trainingSet,50,i, weight, success, error)
+        if i == 0:
+            e0 = e
+        elif i == 90:
+            e1 = e
+        elif i == 180:
+            e2 = e
+        else:
+            e3 =e
 
     c = testing(e0,e1,e2,e3,testDict)
 
